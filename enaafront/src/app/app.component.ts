ca122691aca1667439247eaf9f2c74c5c4de0bd3
mohamed <iaazi.mohamed.1998@gmail.com>
2025-07-31 15:26:15 +0100

    create the navbar

--- enaafront/src/app/app.component.ts
+++ enaafront/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {RenduComponent} from './pages/rendu/rendu.component';
+import {NavbarComponent} from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [
+    RenduComponent,
+    NavbarComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
