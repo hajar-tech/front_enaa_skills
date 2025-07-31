e399c7447be9d324edeeffc84b52e0c7c91cf34d
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-07-31 16:00:39 +0200

    test

--- enaafront/src/app/app.routes.ts
+++ enaafront/src/app/app.routes.ts
@@ -1,3 +1,7 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { ListBriefsComponent } from './pages/list-briefs/list-briefs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     { path : 'briefs', component: ListBriefsComponent }
+];
--- enaafront/src/app/pages/list-briefs/list-briefs.component.ts
+++ enaafront/src/app/pages/list-briefs/list-briefs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 
 @Component({
   selector: 'app-list-briefs',
@@ -6,6 +6,19 @@ import { Component } from '@angular/core';
   templateUrl: './list-briefs.component.html',
   styleUrl: './list-briefs.component.css'
 })
-export class ListBriefsComponent {
+export class ListBriefsComponent implements AfterViewInit{
+
+  ngAfterViewInit(): void {
+    const sidebar = document.getElementById('sidebar')!;
+    const toggleBtn = document.getElementById('toggleBtn')!;
+    const toggleIcon = document.getElementById('toggleIcon')!;
+
+    toggleBtn.addEventListener('click', () => {
+      sidebar.classList.toggle('collapsed');
+      toggleIcon.classList.toggle('fa-chevron-left');
+      toggleIcon.classList.toggle('fa-chevron-right');
+    });
+  }
+
 
 }
